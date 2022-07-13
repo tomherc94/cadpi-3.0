fd9801dd17d0a1098bc542429d10dd91341642e4
Tomas Herculano <tomasherculano@gmail.com>
2022-07-13 13:42:57 -0300

    Arquivo binário gerado

--- master/executerWorkerContainer.sh
+++ master/executerWorkerContainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --cpus=1.0 --net=host --network=mongodb_rede-mongo --hostname $1 tomherc94/worker 
+docker run --cpus=1.0 --network=mongodb_rede-mongo --hostname $1 worker 
 
 
--- master/master.go
+++ master/master.go
@@ -24,8 +24,8 @@ func InitiateMongoClient() *mongo.Client {
 	var err error
 	var client *mongo.Client
 	//uri := "mongodb://localhost:27017"
-	//uri := "mongodb://root:example@localhost:27017/"
-	uri := "mongodb://root:example@mongo_container:27017/"
+	uri := "mongodb://root:example@localhost:27017/"
+	///uri := "mongodb://root:example@mongo_container:27017/"
 	opts := options.Client()
 	opts.ApplyURI(uri)
 	opts.SetMaxPoolSize(5)
